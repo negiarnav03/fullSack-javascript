17c3fd678f7a09314d7f967af4028995c4858f60
negiarnav03 <negijiarnav@gmail.com>
2026-06-03 00:39:48 +0530

    upto theme

--- react/09themeswitcher/src/App.jsx
+++ react/09themeswitcher/src/App.jsx
@@ -1,14 +1,45 @@
-import React, { useState } from 'react'
-
+import React, { useState, useEffect } from 'react'
 import './App.css'
+import { ThemeProvider } from './contexts/theme'
+import ThemeBtn from './components/ThemeButton'
+import Card from './components/card'
 
 function App() {
+  const [themeMode, setThemeMode] = useState("light")
+  
+  const lightTheme = () => {
+    setThemeMode("light")
+  }
+  const darkTheme = () => {
+    setThemeMode("dark")
+  }
   
 
+  // actual change in theme
+
+  useEffect(() => {
+    document.querySelector('html').classList.remove("light", "dark")
+    document.documentElement.classList.add(themeMode) 
+    // better approach beacuse querySelector parse the whole DOM tree 
+    // in case of multiple pages , it will slow the performance
+  }, [themeMode])
+
+
   return (
-    <>
-    <h1>Theme Switcher</h1>
-    </>
+    
+    <ThemeProvider value={{themeMode, lightTheme, darkTheme}}>
+            <div className="flex flex-wrap min-h-screen items-center">
+                <div className="w-full">
+                    <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                        <ThemeBtn/> Toggle Theme
+                    </div>
+
+                    <div className="w-full max-w-sm mx-auto">
+                       <Card/>
+                    </div>
+                </div>
+            </div>
+    </ThemeProvider>
   )
 }
 
